181605f3f68b2c0aecc2bb05befd47d6dcc59335
Mathieu Bautista <mbautista@ircf.fr>
2015-12-01 14:31:55 +0100

    error log : fixed absolute file path

--- functions.php
+++ functions.php
@@ -16,7 +16,7 @@ class PHPMonitoring {
    */
   function init(){
     ini_set('log_errors', 1);
-    ini_set('error_log', self::ERROR_LOG);
+    ini_set('error_log', dirname(__FILE__) . '/' . self::ERROR_LOG);
     global $config;
     if (!file_exists($this->getConfigFilePath())){
       throw new Exception("config file {$this->getConfigFilePath()} not found");
